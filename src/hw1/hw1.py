af7fc37acdda0f8dc733f15d5d4fd131d5c11034
Emma Strubell <strubell@cs.umass.edu>
2014-10-06 11:26:06 -0400

    try 10k mc iters

--- src/hw1/hw1.py
+++ src/hw1/hw1.py
@@ -33,7 +33,7 @@ def estimate(rfunc):
 
         # do Monte-Carlo estimate
         start = time.clock()
-        N = 1000
+        N = 10000
         mc_estimates = [de.samplingEntropyEst(xs[i], N, sigmas[i]) for i in range(trials)]
         elapsed = time.clock()-start
         print "MC estimate %d samples (%d iters): mean %g, std %g (%fms)" % \
