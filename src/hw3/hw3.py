45585f3a51705e9aedc3d4d8a0f48e1c5a26a07a
Emma Strubell <strubell@cs.umass.edu>
2014-10-18 15:02:52 -0400

    fix stuff

--- src/hw3/hw3.py
+++ src/hw3/hw3.py
@@ -7,6 +7,7 @@ from __future__ import division
 import numpy as np
 import ica_tools as ica
 import scipy.io as io
+import scikits.audiolab as audio
 
 data_dir = "dat/"
 fnames = [data_dir + "mix1.mat", data_dir + "mix2.mat", data_dir + "mix3.mat"]
@@ -16,19 +17,19 @@ def get_matlab_array(fname):
 mixes = map(get_matlab_array, fnames)
 
 # play audio
-#soundsc(mixes[0], 44100)
-#soundsc(mixes[1], 44100)
-#soundsc(mixes[2], 44100)
+audio.play(mixes[0], fs=44100)
+audio.play(mixes[1], fs=44100)
+audio.play(mixes[2], fs=44100)
 
 data = np.matrix(np.vstack(mixes))
-rotations = ica.transform(data)
-# rotations = np.concatenate(ica.transform(data), ica.transform(data))
+#rotations = ica.transform(data)
+rotations = np.concatenate(ica.transform(data), ica.transform(data))
 final_rotation_matrix = reduce(np.dot, reversed(rotations))
 print "final rotation matrix:"
 print final_rotation_matrix
 
 # play audio
-# soundsc(data[0,:], 44100)
-# soundsc(data[1,:], 44100)
-# soundsc(data[2,:], 44100)
+audio.play(data[0,:], 44100)
+audio.play(data[1,:], 44100)
+audio.play(data[2,:], 44100)
 
